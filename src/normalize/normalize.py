c6adec44b92f255c891de9db6cf60c04a1b4d708
Matthew Gee <mattgee@gmail.com>
2014-04-25 16:29:03 -0500

    Add normalization module

---
+++ src/normalize/normalize.py
@@ -0,0 +1,46 @@
+
+import pandas as pd 
+import 
+
+"""
+Normalize.py takes weather data as an input
+
+We want to 
+
+"""
+
+
+class WeatherData
+	"""Data, variables, and parameters for weather data.
+	...
+	Args
+	----
+	...
+	Attributes
+	----------
+	...
+	Properties
+	----------
+	...
+	Methods
+	-------
+
+	"""
+	def __init__(self, file):
+		self.
+
+	def hourly_prep():
+		"""
+		Converts hourly data to standard format for use in normalization
+		"""
+
+	def daily_prep():
+		"""
+
+		"""
+
+class EnergyData
+	
+	def 
+
+if 
